7af32f9eca95db101e281b3e3fa09cb0972dbfc7
Jean-Philippe Drapeau <jean-philippe.drapeau@usherbrooke.ca>
2016-03-15 11:23:53 -0400

    create hmi

--- SERVER/deck/labware_module.py
+++ SERVER/deck/labware_module.py
@@ -2,11 +2,13 @@ __author__ = 'Do'
 
 from deck.deck_module import DeckModule
 from deck.deck_module import Coordinate
+from planner_input import setCoord
 
 
 class RectContainerLabware(DeckModule):
 
     def __init__(self, name, coor=Coordinate(0, 0, 0)):
-        super(RectContainerLabware, self).__init__(name, coor)
+        x,y,z = setCoord(name)
+        super(RectContainerLabware, self).__init__(name, coor = Coordinate(x,y,z))
         # Ce contenant imaginaire a un seul puit et on va pipette au milieu a 5 mm 5 mm.
         self.set_well_layout(1, 1, Coordinate(100, 100, 0), Coordinate(0, 0, 0))
--- SERVER/planner_input.py
+++ SERVER/planner_input.py
@@ -1,2 +1,36 @@
-tac_id = input("TAC ID : ")
-pipette_id = input("Pipette ID : ")
+#!/usr/bin/python
+#added for ROS
+import rospy
+import CoordinateMsgs.msg
+from std_msgs.msg import integer
+def setCoord(name):
+    # Function asking user to enter position of each modules
+    print('position for ', name)
+    Fx = raw_input('Enter x coord dumbass: ')
+    Fy = raw_input('Enter y coord dumbass: ')
+    Fz = raw_input('Enter z coord dumbass: ')
+    # Got some error with JSON file, make sure raw_input is an integer so
+    # it can be concanated later.
+    return (int(Fx),int(Fy),int(Fz))
+
+def getPosition():
+    coord_x, coord_y, coord_z = setCoord(name)
+    pub = rospy.Publisher('chatter', CoordinateMsgs)
+    rospy.init_node('talker',anonymous=True)
+    rate = rospy.Rate(10) #10hz
+    msg = CoordinateMsgs()
+    msg.name = name
+    msg.coord_x = coord_x
+    msg.coord_y = coord_y
+    msg.coord_z = coord_z
+
+    while not rospy.is_shutdownd():
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
